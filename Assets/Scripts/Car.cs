b1fbcf1c69aeaec684b17310dabff45c41fd25f0
MachineSpeaker <unknowncoderbase@gmail.com>
2023-03-04 13:21:55 +0530

    Added Steering and UI

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -7,11 +7,19 @@ public class Car : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] float speed = 0f;
     [SerializeField] float speedIncrementPerSecond = 0f;
+    [SerializeField] float turnSpeed = 200f;
+    [SerializeField] int steerValue ;
     // Update is called once per frame
     void Update()
     {
         speed += speedIncrementPerSecond*Time.deltaTime;
+        transform.Rotate(0f, steerValue*turnSpeed*Time.deltaTime, 0f);
         transform.Translate(Vector3.forward * speed* Time.deltaTime);
         
+    }
+    public void Steer(int value)
+    {
+        steerValue = value;
+
     }
 }
