b540df2a454b188681d80e9c26fffbf602ba1798
MachineSpeaker <unknowncoderbase@gmail.com>
2023-03-04 17:57:33 +0530

    Added the MainMenu and Crashing THINGS

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Car : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class Car : MonoBehaviour
     [SerializeField] float speed = 0f;
     [SerializeField] float speedIncrementPerSecond = 0f;
     [SerializeField] float turnSpeed = 200f;
-    [SerializeField] int steerValue ;
+     int steerValue = 0 ;
     // Update is called once per frame
     void Update()
     {
@@ -16,6 +17,17 @@ public class Car : MonoBehaviour
         transform.Rotate(0f, steerValue*turnSpeed*Time.deltaTime, 0f);
         transform.Translate(Vector3.forward * speed* Time.deltaTime);
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene(0);
+            
+        }
+
+
+
     }
     public void Steer(int value)
     {
